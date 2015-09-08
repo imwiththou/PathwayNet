b70fcde2d2a80aa7d867c8801cec65a56fc3d271
Stephen <i@oculus.im>
2015-09-08 15:35:36 +0800

    update Height.py

--- Tryout/Height.py
+++ Tryout/Height.py
@@ -8,7 +8,10 @@ PositionOfTheBall=InitialVelocity*TIME-0.5*AccelerationOfGravity*TIME**2
 
 print "At t=%g s, the height of the ball is %.2f m" %(TIME, PositionOfTheBall)
 
-#alternative way to express the printout sentence
+print 'At time=%.2f s, the height of the ball is %.3f m' % (TIME, PosititonOfTheBall)
+
+#alternative way to express the printout
 
 #failed code below, wait to be checked out
 print 'At t={t:AccelerationOfGravity} s, the height of the ball is {PositionOfTheBall:.2f}m.'.format(t=TIME, PositionOfTheBall=PositionOfTheBall)
+
