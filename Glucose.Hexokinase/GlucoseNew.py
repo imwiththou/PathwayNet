19d792a6a40edd1593dcf11103d81e9653be643c
Stephen <i@oculus.im>
2015-10-14 09:31:25 +0800

    create new model trial

---
+++ Glucose.Hexokinase/GlucoseNew.py
@@ -0,0 +1,93 @@
+#new model
+
+import ode
+
+#Glucose
+
+glucose_vmax = 0.0
+glucose_rate = 1e-07
+glucose_km = 1e-07
+glucose_gamma = 0.0
+
+#Hexokinase
+
+hexokinase_vmax = 0.0
+hexokinase_rate = 1e-07
+hexokinase_km = 1e-07
+hexokinase_gamma = 0.0
+
+#G6P
+
+G6P_vmax = 0.0
+G6P_rate = 1e-07
+G6P_km = 1e-07
+G6P_gamma = 0.0
+
+#Phosphoglucoisomerase
+
+Phosphoglucoisomerase_vmax = 0.0
+Phosphoglucoisomerase_rate = 1e-07
+Phosphoglucoisomerase_km = 1e-07
+Phosphoglucoisomerase_gamma = 0.0
+
+#Fructose-6-phosphate
+
+F6P_vmax = 0.0
+F6P_rate = 1e-07
+F6P_km = 1e-07
+F6P_gamma = 0.0
+
+#Phosphogructokinase
+
+Phosphogructokinase_vmax = 0.0
+Phosphogructokinase_rate = 1e-07
+Phosphogructokinase_km = 1e-07
+Phosphogructokinase_gamma = 0.0
+
+#Fructose-1,6-bisphosphate
+
+F16B_vmax = 0.0
+F16B_rate = 1e-07
+F16B_km = 1e-07
+F16B_gamma = 0.0
+
+#Fructose phosphate aldolase
+
+fructose_phosphate_aldolase_vmax = 0.0
+fructose_phosphate_aldolase_rate = 1e-07
+fructose_phosphate_aldolase_km = 1e-07
+fructose_phosphate_aldolase_gamma = 0.0
+
+#Glyceraldehyde-3-phosphate
+
+G3P_vmax = 0.0
+G3P_rate = 1e-07
+G3P_km = 1e-07
+G3P_gamma = 0.0
+
+#Dihydroxyacetone phosphate
+
+dihydroxyacetone_phosphate_vmax = 0.0
+dihydroxyacetone_phosphate_rate = 1e-07
+dihydroxyacetone_phosphate_km = 1e-07
+dihydroxyacetone_phosphate_gamma = 0.0
+
+#G6P dehydrogenase
+
+G6P_dehydrogenase_vmax = 0.0
+G6P_dehydrogenase_rate = 1e-07
+G6P_dehydrogenase_km = 1e-07
+G6P_dehydrogenase_gamma = 0.0
+
+#6-phosphogluconolactone
+
+phosphogluconolactone_vmax = 0.0
+phosphogluconolactone_rate = 1e-07
+phosphogluconolactone_km = 1e-07
+phosphogluconolactone_gamma = 0.0
+
+
+
+
+y = range()
+
