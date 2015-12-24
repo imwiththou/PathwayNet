6508ad3cdd86486d2bdcea7db476e389413c0482
Stephen <i@oculus.im>
2015-12-24 12:16:20 +0800

    revised RouteA.py and others

---
+++ 2,3-butanediol synthesis/models/RouteA_previousbuild.py
@@ -0,0 +1,126 @@
+import ode
+#The whole pathways networks related with 2,3-butanediol synthesis can be divided into four dependent paths
+#-----------------------------------------------------
+#RouteA: Glu-GA3P-DHAP-G3P
+
+#enzyme setup
+
+GluGA3P_vmax = 0.0
+GluGA3P_rate = 4.14
+GluGA3P_km = 0.451
+GluGA3P_gamma = 0.0
+
+GA3PDHAP_vmax = 0.0
+GA3PDHAP_rate = 564.3791
+GA3PDHAP_km = 6.4539905
+GA3PDHAP_gamma = 0.0
+
+DHAPG3P_vmax = 0.0
+DHAPG3P_rate = 0.48
+DHAPG3P_km = 0.46
+DHAPG3P_gamma = 0.0
+
+#substrate setup
+
+Glu_vmax = 0.0
+Glu_gamma = 0.0
+
+GA3P_gamma = 0.0
+DHAP_gamma = 0.0
+G3P_gamma = 0.0
+
+#ode system setup, reactant and product concentrations input respectively
+
+y = range(7)
+#[GluGA3P]
+y[0] = 4.06e-05
+#[GA3PDHAP]
+y[1] = 1.5e-06
+#[DHAPG3P]
+y[2] = 0.05
+#[Glu]
+y[3] = 1e-06
+#[GA3P]
+y[4] = 0.0
+#[DHAP]
+y[5] = 0.0
+#[G3P]
+y[6] = 0.0
+
+#ode individual definition
+
+def GluGA3P(t, y):
+	production = GluGA3P_vmax
+	degradation = GluGA3P_gamma * y[0]
+	usage = 0
+	return production - degradation - usage
+
+def GA3PDHAP(t, y):
+	production = GA3PDHAP_vmax
+	degradation = GA3PDHAP_gamma * y[1]
+	usage = 0
+	return production - degradation - usage
+	
+def DHAPG3P(t, y):
+	production = DHAPG3P_vmax
+	degradation = DHAPG3P_gamma * y[2]
+	usage = 0
+	return production - degradation - usage
+	
+def Glu(t, y):
+	production = Glu_vmax
+	degradation = Glu_gamma * y[3]
+	usage = (y[3] * y[0] * GluGA3P_rate) / (y[3] + GluGA3P_km)
+	return production - degradation - usage
+	
+def GA3P(t, y):
+	production = (y[3] * y[0] * GluGA3P_rate) / (y[3] + GluGA3P_km)
+	degradation = GA3P_gamma * y[4]
+	usage = (y[4] * y[1] * GA3PDHAP_rate) / (y[4] +GA3PDHAP_km)
+	return production - degradation - usage
+	
+def DHAP(t, y):
+	production = (y[4] * y[1] * GA3PDHAP_rate) / (y[4] +GA3PDHAP_km)
+	degradation = DHAP_gamma * y[5]
+	usage = (y[5] * y[2] * DHAPG3P_rate) / (y[5]+DHAPG3P_km)
+	return production - degradation - usage	
+
+def G3P(t, y):
+	production = (y[5] * y[2] * DHAPG3P_rate) / (y[5]+DHAPG3P_km)
+	degradation = G3P_gamma * y[6]
+	usage = 0.0
+	return production - degradation - usage
+	
+#circuit ODE
+circuitODE = range(7)
+circuitODE[0] = GluGA3P
+circuitODE[1] = GA3PDHAP
+circuitODE[2] = DHAPG3P
+circuitODE[3] = Glu
+circuitODE[4] = GA3P
+circuitODE[5] = DHAP
+circuitODE[6] = G3P
+
+#iteration setup
+
+t0 = 0.0
+tmax = 20000.0
+dt = 0.1
+outfile = 'RouteA_PreviousData.csv'
+f = open(outfile, 'w')
+header = ['time', 'GluGA3P', 'GA3PDHAP', 'DHAPG3P', 'Glu', 'GA3P', 'DHAP', 'G3P']
+f.write(','.join(header) + '\n')
+for x in ode.multirk4(circuitODE, t0, y, dt, tmax):
+    f.write(','.join([str(item) for item in x]) + '\n')
+f.close()
+
+#-----------------------------------------------------
+#RouteB:Glu-GA3P-Pv-alphaAL-DA-AT-BDO
+
+
+#-----------------------------------------------------
+#RouteC:Glu-GA3P-Pv-AA-AT-BDO
+
+
+#-----------------------------------------------------
+#RouteD:Glu-GA3P-Pv-AA-ACT-ETH
--- 2,3-butanediol synthesis/models/RouteB.py
+++ 2,3-butanediol synthesis/models/RouteB.py
@@ -4,159 +4,161 @@ import ode
 
 #enzyme setup
 
-GluGA3Pb_vmax =
-GluGA3Pb_rate =
-GluGA3Pb_km = 
-GluGA3Pb_gamma =
+GluGA3Pb_vmax = 0.0
+GluGA3Pb_rate = 4.14
+GluGA3Pb_km = 0.451
+GluGA3Pb_gamma = 0.0
 
-GA3PPvb_vmax =
-GA3PPvb_rate = 
-GA3PPvb_km = 
-GA3PPvb_gamma =
+#not confirmed yet
 
-PvAlphaALb_vmax =
-PvAlphaALb_rate =
-PvAlphaALb_km =
-PvAlphaALb_gamma =
+GA3PPvb_vmax = 0.51
+GA3PPvb_rate = 4.14
+GA3PPvb_km = 0.7
+GA3PPvb_gamma = 0.0
 
-AlphaAlDAb_vmax =
-AlphaAlDAb_rate =
-AlphaAlDAb_km =
-AlphaAlDAb_gamma =
+PvAlphaALb_vmax = 0
+PvAlphaALb_rate = 21.3
+PvAlphaALb_km = 0.92
+PvAlphaALb_gamma = 0.0
 
-DAATb_vmax = 
-DAATb_rate =
-DAATb_km =
-DAATb_gamma =
+AlphaALDAb_vmax = 0.9
+AlphaALDAb_rate = 21.5
+AlphaALDAb_km = 0.672
+AlphaALDAb_gamma = 0.0
 
-ATBDOb_vmax =
-ATBDOb_rate =
-ATBDOb_km =
-ATBDOb_gamma =
+DAATb_vmax = 0
+DAATb_rate = 59.1
+DAATb_km = 0.77
+DAATb_gamma = 0.0
+
+ATBDOb_vmax = 8.2
+ATBDOb_rate = 0.11
+ATBDOb_km = 0.732
+ATBDOb_gamma = 0.0
 
 
 #substrate setup
 
-Glub_vmax =
-Glub_gamma =
+Glub_vmax = 0
+Glub_gamma = 0
 
-GA3Pb_gamma =
-Pvb_gamma =
-AlphaALb_gamma =
-DAb_gamma =
-ATb_gamma =
-BDOb_gamma =
+GA3Pb_gamma = 0
+Pvb_gamma = 0
+AlphaALb_gamma = 0
+DAb_gamma = 0
+ATb_gamma = 0
+BDOb_gamma = 0
 
 
 #ODE system setup, reactant and product concentrations input respectively
 
 y = range(13)
 #[GluGA3Pb]
-y[0] = 
+y[0] = 4.07e-05
 #[GA3PPvb]
-y[1] =
-#[PvAlphaALb]
-y[2] =
+y[1] = 1.5e-05
+#[PvAlphaALb], i.e. acetolactate synthase
+y[2] = 0.03
 #[AlphaALDAb]
-y[3] =
+y[3] = 1e-06
 #[DAATb]
-y[4] =
+y[4] = 1e-06
 #[ATBDOb]
-y[5] =
+y[5] = 1.2e-06
 
 #[Glub]
-y[6] =
+y[6] = 1e-05
 #[GA3Pb]
-y[7] =
+y[7] = 0
 #[Pvb]
-y[8] =
+y[8] = 0
 #[AlphaALb]
-y[9] =
+y[9] = 0
 #[DAb]
-y[10] =
+y[10] = 0
 #[ATb]
-y[11] =
+y[11] = 0
 #[BDOb]
-y[12] =
+y[12] = 0
 
 #individual definition
 
 def GluGA3Pb(t, y):
 	production = GluGA3Pb_vmax
-	degradation = GluGA3Pb_gamma * [GluGA3Pb]
+	degradation = GluGA3Pb_gamma * y[0]
 	usage = 0.0 
 	return production - degradation - usage
 
 def GA3PPvb(t, y):
 	production = GA3PPvb_vmax
-	degradation = GA3PPvb_gamma * [GA3PPvb]
+	degradation = GA3PPvb_gamma * y[1]
 	usage = 0.0
 	return production - degradation - usage
 
 def PvAlphaALb(t, y):
 	production = PvAlphaALb_vmax
-	degradation = PvAlphaALb_gamma * [PvAlphaALb]
+	degradation = PvAlphaALb_gamma * y[2]
 	usage = 0.0
 	return production - degradation - usage
 	
 def AlphaALDAb(t, y):
 	production = AlphaALDAb_vmax
-	degradation = AlphaALDAb_gamma * [AlphaALDAb]
+	degradation = AlphaALDAb_gamma * y[3]
 	usage = 0.0
 	return production - degradation - usage
 	
 def DAATb(t, y):
 	production = DAATb_vmax
-	degradation = DAATb_gamma * [DAATb]
+	degradation = DAATb_gamma * y[4]
 	usage = 0.0
 	return production - degradation - usage
 	
 def ATBDOb(t, y):
 	production = ATBDOb_vmax
-	degradation = ATBDOb_gamma * [ATBDOb]
+	degradation = ATBDOb_gamma * y[5]
 	usage = 0.0
 	return production - degradation - usage
 	
 
 def Glub(t, y):
 	production = Glub_vmax
-	degradation = Glub_gamma * [Glub]
-	usage = ([Glub] * [GA3Pb] * GluGA3Pb_rate)/([Glub] + GluGA3Pb_km)
+	degradation = Glub_gamma * y[6]
+	usage = (y[6] * y[7] * GluGA3Pb_rate)/(y[6] + GluGA3Pb_km)
 	return production - degradation - usage
 	
 def GA3Pb(t, y):
-	production = ([Glub] * [GA3Pb] * GluGA3Pb_rate)/([Glub] + GluGA3Pb_km)
-	degradation = GA3Pb_gamma * [GA3Pb]
-	usage = ([GA3Pb] * [Pvb] * GA3PPvb_rate)/([GA3P] + GA3PPvb_km)
+	production = (y[6] * y[7] * GluGA3Pb_rate)/(y[6] + GluGA3Pb_km)
+	degradation = GA3Pb_gamma * y[7]
+	usage = (y[7] * y[8] * GA3PPvb_rate)/(y[7] + GA3PPvb_km)
 	return production - degradation - usage
 	
 def Pvb(t, y):
-	production = ([GA3Pb] * [Pvb] * GA3PPvb_rate)/([GA3P] + GA3PPvb_km)
-	degradation = Pvb_gamma * [Pvb]
-	usage = ([Pvb] * [AlphaALb] * PvbAlphaALb_rate)/([Pvb] + PvbAlphaALb_km)
+	production = (y[7] * y[8] * GA3PPvb_rate)/(y[7] + GA3PPvb_km)
+	degradation = Pvb_gamma * y[8]
+	usage = (y[8] * y[9] * PvAlphaALb_rate)/(y[8] + PvAlphaALb_km)
 	return production - degradation - usage
 	
 def AlphaALb(t, y):
-	production = ([Pvb] * [AlphaALb] * PvbAlphaALb_rate)/([Pvb] + PvbAlphaALb_km)
-	degradation = AlphaALb_gamma * [AlphaALb]
-	usage = ([AlphaALb] * [DAb] * AlphaALDAb_rate)/([AlphaALb] + AlphaALDAb_km)
+	production = (y[8] * y[9] * PvAlphaALb_rate)/(y[8] + PvAlphaALb_km)
+	degradation = AlphaALb_gamma * y[9]
+	usage = (y[9] * y[10] * AlphaALDAb_rate)/(y[9] + AlphaALDAb_km)
 	return production - degradation - usage
 	
 def DAb(t, y):
-	production = ([AlphaALb] * [DAb] * AlphaALDAb_rate)/([AlphaALb] + AlphaALDAb_km)
-	degradation = DAb_gamma * [DAb]
-	usage = ([DAb] * [ATb] * DAATb_rate)/([DAb] + DAATb_km)
+	production = (y[9] * y[10] * AlphaALDAb_rate)/(y[9] + AlphaALDAb_km)
+	degradation = DAb_gamma * y[10]
+	usage = (y[10] * y[11] * DAATb_rate)/(y[10] + DAATb_km)
 	return production - degradation - usage
 	
 def ATb(t, y):
-	production = ([DAb] * [ATb] * DAATb_rate)/([DAb] + DAATb_km)
-	degradation = ATb_gamma * [ATb]
-	usage = ([ATb] * [BDOb] * ATBDOb_rate)/([ATb] + ATBDOb_km)
+	production = (y[10] * y[11] * DAATb_rate)/(y[10] + DAATb_km)
+	degradation = ATb_gamma * y[11]
+	usage = (y[11] * y[12] * ATBDOb_rate)/(y[11] + ATBDOb_km)
 	return production - degradation - usage
 	
 def BDOb(t, y):
-	production = ([ATb] * [BDOb] * ATBDOb_rate)/([ATb] + ATBDOb_km)
-	degradation = BDOb_gamma * [BDOb]
+	production = (y[11] * y[12] * ATBDOb_rate)/(y[11] + ATBDOb_km)
+	degradation = BDOb_gamma * y[12]
 	usage = 0.0
 	return production - degradation - usage
 
